990321ef06dc0f4b9912b8e191c3790a10fe8242
Renzi Fidele <renzifidele2001@gmail.com>
2025-01-26 02:07:45 +0200

    feat: Adicionando link dos produtos ao botão de comprar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,9 @@ const data = [
       titulo: "Izzygreen",
       foto: "https://iili.io/2LFT5jR.jpg",
       preco: 100,
-      download: 360,
+      download: 211,
       descricao: "HD video, Novo vasamento, Mais de 300 videos, 600 fotos, conteúdo quente",
+      link: "https://www.paypal.com/ncp/payment/CJCTPW3J7326G",
    },
    {
       titulo: "Emmi Saller",
@@ -15,41 +16,47 @@ const data = [
       preco: 35,
       download: 360,
       descricao: "Full HD,  mais de 500 videos, 300 fotos, 100Gb",
+      link: "https://www.paypal.com/ncp/payment/F3UVFYZCDFNGS",
    },
    {
       titulo: "Sophie Rain",
       foto: "https://iili.io/2LFTRTv.jpg",
       preco: 40,
-      download: 360,
+      download: 869,
       descricao: "Garota linda, 365 arquivos, 85 videos full Hd, 387 fotos, 87GB",
+      link: "https://www.paypal.com/ncp/payment/WT68S63GFCD4A",
    },
    {
       titulo: "Ivanka",
       foto: "https://iili.io/2LFTwn2.jpg",
       preco: 25,
-      download: 360,
+      download: 411,
       descricao: "HD video, 85 arquivos, 59 videos, 76 fotos 35GB",
+      link: "https://www.paypal.com/ncp/payment/QP2XHUD9R7MHG",
    },
    {
       titulo: "Snapgod full",
       foto: "https://iili.io/2LFTNGS.jpg",
       preco: 65,
-      download: 360,
+      download: 1100,
       descricao: "Pasta exclusiva com mais de 900 video,700 fotos videos HD 1000GB",
+      link: "https://www.paypal.com/ncp/payment/QP2XHUD9R7MHG",
    },
    {
       titulo: "Kazumi Squirt",
       foto: "https://iili.io/2LFT13X.jpg",
       preco: 100,
-      download: 360,
+      download: 799,
       descricao: "sexy Girl, pasta rara, 3035 arquivos, 265 videos, 2967 fotos 780GB",
+      link: "https://abrir.link/DvnYF",
    },
    {
       titulo: "Brookeblond",
       foto: "https://iili.io/2LFT7Qp.jpg",
       preco: 35,
-      download: 360,
-      descricao: "sexy girl, pasta rara, mais de 200 videos, 500 fotos, 398 arquivos, 50GB",
+      download: 1230,
+      descricao: "Sexy girl, pasta rara, mais de 200 videos, 500 fotos, 398 arquivos, 50GB",
+      link: "https://www.paypal.com/ncp/payment/323H9T8EGYB2N",
    },
    {
       titulo: "Bella Ramos",
@@ -57,13 +64,15 @@ const data = [
       preco: 15,
       download: 360,
       descricao: "Videos altamente excitatorios, 100 arquivos, 70 videos, 65 fotos, 35Gb",
+      link: "https://www.paypal.com/ncp/payment/XUCZDP4TNFGYA",
    },
    {
       titulo: "Streamer Girls",
       foto: "https://iili.io/2LFTguj.jpg",
       preco: 80,
-      download: 360,
+      download: 73,
       descricao: "Pasta com mais de 900 videos, 3000 fotos 765 arquivos, mais de 15 modelos diferentes, qualidade HD, 1000GB",
+      link: "https://www.paypal.com/ncp/payment/LNKHPDR766VJU",
    },
 ];
 
@@ -73,6 +82,7 @@ const Home = () => {
    // TODO: Adicionar funcionalidade de filtragem dos videos
    // TODO: Adicionar estrutura basica do ambiente fullstack
    // TODO: Adicionar funcionalidade de gerenciamento do conteudo do site pelo Admin(Chance)
+   // TODO: Mostrar notificação de compra do produto no intervalo de 10sec
    return (
       <Container fluid>
          <Slides />
@@ -85,8 +95,8 @@ const Home = () => {
                {/* Vídeos */}
                <div className="px-5 mx-5">
                   <Row className="justify-content-center g-4 px-5 mx-auto mt-4">
-                     {data.map(({ descricao, foto, titulo, download }, k) => (
-                        <Col md={4} key={k}>
+                     {data.map(({ descricao, foto, titulo, download, link }, k) => (
+                        <Col md={3} key={k}>
                            <Card className="h-100 text-bg-dark border border-light">
                               <Card.Img variant="top" src={foto} />
                               <Card.Body>
@@ -95,13 +105,13 @@ const Home = () => {
                               </Card.Body>
                               <Card.Footer className="border-secondary d-flex justify-content-between align-items-center">
                                  <span className="fw-semibold">
-                                    <i className="bi bi-download"></i> {parseFloat(download).toFixed(2)}
+                                    <i className="bi bi-download"></i> {download}
                                  </span>
                                  <div className="d-flex gap-2 justify-content-end">
-                                    <Button>
+                                    <Button size="sm" as="a" target="_blank" href={link}>
                                        <i className="bi bi-cart"></i> Comprar
                                     </Button>
-                                    <Button variant="outline-light">
+                                    <Button size="sm" variant="outline-light">
                                        <i className="bi bi-info-circle"></i> Ver detalhes
                                     </Button>
                                  </div>
