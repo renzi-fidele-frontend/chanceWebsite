0a71d1b87bd2e7e64cbb06d22357ce831e5ce1b5
Renzi Fidele <renzifidele2001@gmail.com>
2025-01-31 02:27:15 +0200

    feat: Renderizando os videos a serem vendidos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -70,6 +70,18 @@ const data = [
       download: 411,
       descricao: "HD video, 85 arquivos, 59 videos, 76 fotos 35GB",
       link: "https://www.paypal.com/ncp/payment/QP2XHUD9R7MHG",
+      previas: [
+         "https://iili.io/2ZJxrMP.jpg",
+         "https://iili.io/2ZJxgoB.jpg",
+         "https://iili.io/2ZJxStV.jpg",
+         "https://iili.io/2ZJx8wQ.jpg",
+         "https://iili.io/2ZJx4P1.jpg",
+         "https://iili.io/2ZJxPKF.jpg",
+         "https://iili.io/2ZJxicg.jpg",
+         "https://iili.io/2ZJxsSa.jpg",
+         "https://iili.io/2ZJxQHJ.jpg",
+         "https://iili.io/2ZJxZAv.jpg",
+      ],
    },
    {
       titulo: "Snapgod full",
@@ -78,6 +90,18 @@ const data = [
       download: 1100,
       descricao: "Pasta exclusiva com mais de 900 video,700 fotos videos HD 1000GB",
       link: "https://www.paypal.com/ncp/payment/QP2XHUD9R7MHG",
+      previas: [
+         "https://iili.io/2ZJT4PS.jpg",
+         "https://iili.io/2ZJTvAG.jpg",
+         "https://iili.io/2ZJT8Nf.jpg",
+         "https://iili.io/2ZJTil9.jpg",
+         "https://iili.io/2ZJTsSe.jpg",
+         "https://iili.io/2ZJTQHu.jpg",
+         "https://iili.io/2ZJTZAb.jpg",
+         "https://iili.io/2ZJTDDx.jpg",
+         "https://iili.io/2ZJTmoQ.jpg",
+         "https://iili.io/2ZJTpVV.jpg",
+      ],
    },
    {
       titulo: "Kazumi Squirt",
@@ -86,14 +110,38 @@ const data = [
       download: 799,
       descricao: "sexy Girl, pasta rara, 3035 arquivos, 265 videos, 2967 fotos 780GB",
       link: "https://abrir.link/DvnYF",
+      previas: [
+         "https://iili.io/2ZJ5Hf2.jpg",
+         "https://iili.io/2ZJRpV4.jpg",
+         "https://iili.io/2ZJRDDG.jpg",
+         "https://iili.io/2ZJRmxf.jpg",
+         "https://iili.io/2ZJ5dU7.webp",
+         "https://iili.io/2ZJ5KOu.jpg",
+         "https://iili.io/2ZJ5JlS.jpg",
+         "https://iili.io/2ZJ53J9.jpg",
+         "https://iili.io/2ZJ5Bxj.webp",
+         "https://iili.io/2ZJ5niQ.jpg",
+      ],
    },
    {
-      titulo: "Brookeblond",
+      titulo: "Brookeblond: Sexy girl",
       foto: "https://iili.io/2LFT7Qp.jpg",
       preco: 35,
       download: 1230,
       descricao: "Sexy girl, pasta rara, mais de 200 videos, 500 fotos, 398 arquivos, 50GB",
       link: "https://www.paypal.com/ncp/payment/323H9T8EGYB2N",
+      previas: [
+         "https://iili.io/2ZJ7r3N.jpg",
+         "https://iili.io/2ZJ7PyX.jpg",
+         "https://iili.io/2ZJ76vt.jpg",
+         "https://iili.io/2ZJ74aI.jpg",
+         "https://iili.io/2ZJ7Ljs.jpg",
+         "https://iili.io/2ZJ7DG4.jpg",
+         "https://iili.io/2ZJ7b6l.jpg",
+         "https://iili.io/2ZJYccN.jpg",
+         "https://iili.io/2ZJY7PR.jpg",
+         "https://iili.io/2ZJYGNn.jpg",
+      ],
    },
    {
       titulo: "Bella Ramos",
@@ -102,23 +150,46 @@ const data = [
       download: 360,
       descricao: "Videos altamente excitatorios, 100 arquivos, 70 videos, 65 fotos, 35Gb",
       link: "https://www.paypal.com/ncp/payment/XUCZDP4TNFGYA",
+      previas: [
+         "https://iili.io/2ZdHLoG.jpg",
+         "https://iili.io/2ZdHits.jpg",
+         "https://iili.io/2ZdHPNn.jpg",
+         "https://iili.io/2ZdHQVf.jpg",
+         "https://iili.io/2ZdHZP4.jpg",
+         "https://iili.io/2ZdHbl2.jpg",
+         "https://iili.io/2ZdJ9R9.jpg",
+         "https://iili.io/2ZdHmSS.jpg",
+         "https://iili.io/2ZdJJDu.jpg",
+         "https://iili.io/2ZdJ2ob.jpg",
+      ],
    },
    {
-      titulo: "Streamer Girls",
+      titulo: "Fanbus",
       foto: "https://iili.io/2LFTguj.jpg",
       preco: 80,
       download: 73,
       descricao: "Pasta com mais de 900 videos, 3000 fotos 765 arquivos, mais de 15 modelos diferentes, qualidade HD, 1000GB",
       link: "https://www.paypal.com/ncp/payment/LNKHPDR766VJU",
+      previas: [
+         "https://iili.io/2ZdKR8Q.jpg",
+         "https://iili.io/2ZdKAcx.jpg",
+         "https://iili.io/2ZdKuFj.jpg",
+         "https://iili.io/2ZdKI6b.jpg",
+         "https://iili.io/2ZdK79V.jpg",
+         "https://iili.io/2ZdKYAB.jpg",
+         "https://iili.io/2ZdKawP.jpg",
+         "https://iili.io/2ZdKct1.webp",
+         "https://iili.io/2ZdK0oF.jpg",
+         "https://iili.io/2ZdK1Mg.jpg",
+      ],
    },
 ];
 
 const Home = () => {
-   // TODO: Adicionar estrutura basica do Footer
-   // TODO: Renderizar os videos a serem vendidos
    // TODO: Adicionar funcionalidade de filtragem dos videos
    // TODO: Adicionar estrutura basica do ambiente fullstack
    // TODO: Adicionar funcionalidade de gerenciamento do conteudo do site pelo Admin(Chance)
+
    // TODO: Mostrar notificação de compra do produto no intervalo de 10sec
    return (
       <Container fluid>
