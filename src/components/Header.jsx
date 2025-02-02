7e535b01aec11ad92a47ee31f2d5ed2d29c81369
Renzi Fidele <renzifidele2001@gmail.com>
2025-02-03 00:26:24 +0200

    feat: Internacionalizando o cabeçalho para inglês

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import { Button, Container, Image, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import styles from "./Header.module.css";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+   const { t } = useTranslation();
+   const { linkText } = t("header");
    return (
       <Container fluid className="bg-black">
          <div className="container d-flex align-items-center justify-content-between w-100 py-3">
@@ -12,7 +15,7 @@ const Header = () => {
             </Navbar.Brand>
 
             <Button as={Link} to="/how_to_buy" variant="success">
-               Como comprar?
+               {linkText}
             </Button>
 
             {/* TODO: Adicionar funcionalidade de alternar o tema entre dark/light */}
--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -4,3 +4,5 @@ import DetectorDeLinguagem from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(Backend).use(DetectorDeLinguagem).use(initReactI18next).init({ returnObjects: true, fallbackLng: "en", debug: true });
+
+export default i18n;
