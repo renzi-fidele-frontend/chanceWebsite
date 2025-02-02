3f395f00291ef1fa32eba622d8fd13b26bc32340
Renzi Fidele <renzifidele2001@gmail.com>
2025-02-03 01:08:30 +0200

    feat: Internacionalizando o componente de rodapé para inglês

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,24 @@
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "./Footer.module.css";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+   const { t } = useTranslation();
+   const { p, contact, duvidas } = t("footer");
    return (
       <Container className="py-4 px-5 border border-top mt-5 bg-black">
          <Row className="mb-4 mb-md-0">
             <Col className="text-center text-md-start" md={6}>
                <Image id={styles.logo} src={logo} alt="Logo do site" />
-               <p className="mb-0 mt-2">Explore conteúdos exclusivos e feitos para você.</p>
+               <p className="mb-0 mt-2">{p}</p>
             </Col>
             <Col className="mt-3 mt-md-0 text-center text-md-end">
                <hr className="d-md-none" />
-               <h3 className="mb-3 mb-md-2">Dúvidas online</h3>
+               <h3 className="mb-3 mb-md-2">{duvidas}</h3>
                <a target="_blank" href="https://t.me/+RcEFSoRa5LwyMWM0">
                   <Button style={{ backgroundColor: "#229ED9" }}>
-                     <i className="bi bi-telegram me-2"></i> Contatar no Telegram
+                     <i className="bi bi-telegram me-2"></i>{contact}
                   </Button>
                </a>
             </Col>
