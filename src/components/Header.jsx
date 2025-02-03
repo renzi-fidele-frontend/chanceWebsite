5db89cd30763edaab38f06ab01de7c0f17171d00
Renzi Fidele <renzifidele2001@gmail.com>
2025-02-03 02:33:05 +0200

    feat: Adicionando a funcionalidade de mudar de idioma ao se clicar no botão

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,20 @@ import logo from "../assets/logo.png";
 import styles from "./Header.module.css";
 import { useTranslation } from "react-i18next";
 import { ContextValue } from "../context/Provider";
+import i18n from "../i18n/i18n";
 
 const Header = () => {
    const { estado, dispatch } = ContextValue();
    const { t } = useTranslation();
    const { linkText } = t("header");
+
+   i18n.on("languageChanged", (idiomaNovo) => {
+      dispatch({ type: "setIdioma", payload: idiomaNovo });
+   });
+   function mudarIdioma(idiomaNovo) {
+      i18n.changeLanguage(idiomaNovo);
+   }
+
    return (
       <Container fluid className="bg-black">
          <div className="container d-flex align-items-center justify-content-between w-100 py-3">
@@ -26,8 +35,22 @@ const Header = () => {
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                     <Dropdown.Item active={estado.idioma === "en"}>🇬🇧 English</Dropdown.Item>
-                     <Dropdown.Item active={estado.idioma === "de"}>🇩🇪 Deutsch</Dropdown.Item>
+                     <Dropdown.Item
+                        active={estado.idioma === "en"}
+                        onClick={() => {
+                           mudarIdioma("en");
+                        }}
+                     >
+                        🇬🇧 English
+                     </Dropdown.Item>
+                     <Dropdown.Item
+                        active={estado.idioma === "de"}
+                        onClick={() => {
+                           mudarIdioma("de");
+                        }}
+                     >
+                        🇩🇪 Deutsch
+                     </Dropdown.Item>
                   </Dropdown.Menu>
                </Dropdown>
             </div>
