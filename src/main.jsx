320a75f57e9aea9fc22cc7da45ed09f9b80af3b4
Renzi Fidele <renzifidele2001@gmail.com>
2025-02-07 05:04:10 +0200

    feat: Configurando a visualização de métricas no site

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Router from "./router/Router";
 import i18n from "./i18n/i18n";
 import Provider from "./context/Provider";
 import PreLoader from "./components/PreLoader";
+import { Analytics } from "@vercel/analytics/react";
 
 // TODO: Adicionar modal com blur confirmando se o cliente é maior de 18 anos
 createRoot(document.getElementById("root")).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")).render(
          <Provider>
             <Router />
          </Provider>
+         <Analytics />
       </Suspense>
    </StrictMode>
 );
